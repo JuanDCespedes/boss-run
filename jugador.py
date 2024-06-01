3227d3c2fd95fdf1a32e7ee396ec197f78fd831b
JuanDCespedes <jdcu2005@gmail.com>
2024-06-01 12:58:22 -0500

    tienda y barra de vida xd

--- jugador.py
+++ jugador.py
@@ -3,7 +3,6 @@ from pygame.locals import *
 from imagenes1 import *
 
 class Jugador():    
-    
     def __init__(self):
         self.contador_j = 0
         self.jugador = jugador_caminar[self.contador_j]
@@ -15,6 +14,11 @@ class Jugador():
         self.direccion = "d"
         self.saltando = False
         self.contador_salto = 0
+        self.vida = 20  
+        self.monedas = 1  
+        self.hp = 0  
+        self.atk = 0  
+        self.vel = 0  
     
     def mover(self):
         teclas = pygame.key.get_pressed()
@@ -100,3 +104,25 @@ class Jugador():
             else:
                 self.saltando = False
                 self.contador_salto = 0
+
+    def recibir_dano(self, cantidad):
+        self.vida -= cantidad
+        if self.vida < 0:
+            self.vida = 0
+
+    def dibujar_vida(self, screen):
+        largo_barra = 200
+        ancho_barra = 20
+        borde_barra = 2
+
+        vida_actual = int((self.vida / 20) * largo_barra)
+        
+     
+        pygame.draw.rect(screen, (255, 0, 0), (10, 10, largo_barra, ancho_barra))
+        pygame.draw.rect(screen, (0, 255, 0), (10, 10, vida_actual, ancho_barra))
+        pygame.draw.rect(screen, (255, 255, 255), (10, 10, largo_barra, ancho_barra), borde_barra)
+        
+        
+        font = pygame.font.SysFont("ravie", 24)  
+        texto_vida = font.render(f"{self.vida}/20", True, (0, 0, 255))  
+        screen.blit(texto_vida, (10, 35))
